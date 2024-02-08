2b453fe2280016e5d5083ce61c1932c6ce9a4252
김세희 <sadie1001@naver.com>
2024-02-08 16:12:51 +0900

    chore: 필요없는 코드 부분 지움

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-// Implement smooth scrolling for anchor links
-document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth",
-    });
-  });
-});
+//script for something
