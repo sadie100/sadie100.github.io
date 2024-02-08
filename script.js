8e01f3adc5ea565981ef2dafc03f7f62ded7da11
김세희 <sadie1001@naver.com>
2024-02-08 18:06:36 +0900

    feat: 템플릿 작성, arrow button 및 사이드바 구현

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-//script for something
+document.addEventListener("DOMContentLoaded", () => {
+  const scrollToNextSection = (currentSection) => {
+    const nextSection = currentSection.nextElementSibling;
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  const scrollToFirstSection = () => {
+    const firstSection = document.querySelector(".section");
+    if (firstSection) {
+      firstSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+  // Add click event to each arrow icon
+  document.querySelectorAll(".next-section-arrow").forEach((arrow) => {
+    arrow.addEventListener("click", () => {
+      const currentSection = arrow.closest(".section");
+      scrollToNextSection(currentSection);
+    });
+  });
+
+  // Add click event to last arrow icon
+  document
+    .querySelector(".first-section-arrow")
+    .addEventListener("click", () => {
+      scrollToFirstSection();
+    });
+});
+
+// Smooth scroll for sidebar navigation
+document.querySelectorAll('nav a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    let target = document.querySelector(this.getAttribute("href"));
+    target.scrollIntoView({ behavior: "smooth" });
+  });
+});
